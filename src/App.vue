55b3cd6492af02d809c2005bb20d97742165ca34
Martina Kolářová <ruzickova1.martina@gmail.com>
2025-11-23 22:43:14 +0100

    First version of Vue project

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { ref } from 'vue';
+import Modal from './components/Modal.vue';
+const title = ref('My first Vue app');
+const name = ref(null);
+const appText = 'Mám ráda červenou?';
+const red = ref('red');
+
+function handleClick() {
+  console.log(name.value);
+  name.value.classList.add('active');
+}
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+  <Modal :header="appText" :theme="'red'" />
+  <button @click="handleClick">Click me</button>
+
+  <input type="text" ref="name" />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px, solid, #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
+}
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,43 @@
+<script setup>
+const props = defineProps({
+  header: String,
+  theme: String,
+});
+</script>
+<template>
+  <div class="backdrop">
+    <div class="modal" :class="{ red: theme === 'red' }">
+      <h1>{{ props.header }}</h1>
+      <p>{{ props.theme }}</p>
+    </div>
+  </div>
+</template>
+<style>
+.modal {
+  width: 400px;
+  padding: 20px;
+  margin: 100px auto;
+  background: white;
+  border-radius: 10px;
+}
+.backdrop {
+  left: 0;
+  top: 0;
+  position: fixed;
+  background: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  height: 100%;
+}
+.modal h1 {
+  color: mediumturquoise;
+  border: none;
+  padding: 0;
+}
+.modal.red {
+  background: crimson;
+  color: white;
+}
+.modal.red h1 {
+  color: white;
+}
+</style>
